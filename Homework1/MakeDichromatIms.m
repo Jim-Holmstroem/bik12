d2ffa1990a5d667afd3478b956de395ffc69e641
Jim Holmström <jim.holmstroem@gmail.com>
2012-03-21 10:46:30 +0100

    Refactored and tried to find the bug

--- Homework1/MakeDichromatIms.m
+++ Homework1/MakeDichromatIms.m
@@ -3,30 +3,48 @@ function [ pim, dim ] = MakeDichromatIms( im )
     %   Detailed explanation goes here
     im = double(im);
     im_trans = (im./255).^2.2;
-    %Protanopes
-    im_prot = 0.992052*im_trans + 0.003974;
-    %Deuteranopes
-    im_deut = 0.957237*im_trans + 0.0213814;
-    M = [17.8824 43.5161 4.11935; 3.45565 27.1554 3.86714; 0.0299566 0.184309 1.46709];
-    P = [0 2.02344 -2.52581; 0 1 1; 0 0 1];
-    D = [1 0 0; 0.494207 0 1.24827; 0 0 1];
-    pim = zeros(size(im_prot));
-    dim = zeros(size(im_deut));
     
+    im_p = 0.992052*im_trans + 0.003974;
+    im_d = 0.957237*im_trans + 0.0213814;
+    
+    M = [  17.8824  43.5161  4.11935; 
+           3.45565  27.1554  3.86714;
+         0.0299566 0.184309  1.46709];
+    
+    P = [        0  2.02344 -2.52581; 
+                 0        1        1; 
+                 0        0        1];
+
+    D = [        1        0        0;
+          0.494207        0  1.24827; 
+                 0        0        1];
+    
+    pim = zeros(size(im_p));
+    dim = zeros(size(im_d));
+    
+    %Could use these but have got some numerical roundof errors 
     M1DM=M^(-1)*D*M; %Precalc transformation
     M1PM=M^(-1)*P*M;
-    
+
     for it = 1:size(im,1)
         for jt = 1:size(im,2)
-            dim(it,jt,:) = M1DM*reshape(im_deut(it,jt,:),3,1);
-            pim(it,jt,:) = M1PM*reshape(im_prot(it,jt,:),3,1);
+            pim(it,jt,:) = M^(-1)*(P*(M*reshape(im_p(it,jt,:),3,1)));
+            dim(it,jt,:) = M^(-1)*(D*(M*reshape(im_d(it,jt,:),3,1)));
         end
     end
-    
-    minminpim = min(min(pim))
-    
+
+    minpim=min(min(min(pim)))
+    if(minpim<0)
+        pim=pim-minpim;
+    end
+    mindim=min(min(min(dim)))
+    if(mindim<0)
+        dim=dim-mindim;
+    end
+
     pim = 255*(pim.^(1/2.2));
     dim = 255*(dim.^(1/2.2));
     pim = uint8(pim);
     dim = uint8(dim);
+
 end
