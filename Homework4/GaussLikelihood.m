df3d7225a6030a7798c0d366fa22a07ce1fdb747
Jim Holmström <jim.holmstroem@gmail.com>
2012-04-04 00:24:12 +0200

    Started trying to apply likelihoodfunction, but got some problems trying to workaround loops

--- Homework4/GaussLikelihood.m
+++ Homework4/GaussLikelihood.m
@@ -1,4 +1,6 @@
 function lvals = GaussLikelihood(xs,mean,Sigma)
     [N,d]=size(xs);
-    dxs=xs-mean;
-    lvals=1/((2*pi)^(d/2)*det(Sigma)^(1/2))*exp(-1/2*dxs'*inv(Sigma)*dxs)
+    dxs=xs-repmat(mean,[N 1]);
+    
+
+    lvals=arrayfun(@(dx) 1/((2*pi)^(d/2)*det(Sigma)^(1/2))*exp(-1/2*dx*inv(Sigma)*dx'),dxs)
--- Homework4/run.m
+++ Homework4/run.m
@@ -1,3 +1,10 @@
 [mean,Sigma]=TrainColourModel('George_W_Bush');
 
+img=double(imread('StudentImages/IMG_0256.jpg'));
+xs=reshape(img,[size(img,1)*size(img,2),3]);
+
+lvals=GaussLikelihood(xs,mean,Sigma);
+
+img_skin=reshape(lvals,[size(img,1),size(img,2)])
+
 
