7998badc0e6f1a707a90e2d38924d7a5aff21d5b
Jim Holmström <jim.holmstroem@gmail.com>
2012-04-14 11:52:07 +0200

    Saves the plot automatically and doesnt even display figure

--- Homework5/doc/plotter.m
+++ Homework5/doc/plotter.m
@@ -2,7 +2,8 @@
 c1=[7, 11;15, 9;15, 7;13, 5;14, 4;9, 3;11, 3];
 c2=[11, 11;13, 11;8, 10;9, 9;7, 7;7, 5;15, 3];
 
-plot(c1(:,1),c1(:,2),'rx','LineWidth',1);
+figure('visible','off')
+h=plot(c1(:,1),c1(:,2),'rx','LineWidth',1);
 hold on;
 plot(c2(:,1),c2(:,2),'bs','LineWidth',1);
 
@@ -14,4 +15,6 @@ plot([13 13 16 18],[0 2 5 5],'k','LineWidth',1);%upperleft boundary
 axis([2 18 0 14])
 xlabel('x_1')
 ylabel('x_2')
+
+saveas(h,'exercise2','pdf')
 
