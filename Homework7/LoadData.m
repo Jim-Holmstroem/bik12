9e651424642c453772ef36c699d566886baa83ac
Jim Holmström <jim.holmstroem@gmail.com>
2012-04-19 15:06:46 +0200

    Finished

--- Homework7/LoadData.m
+++ Homework7/LoadData.m
@@ -4,7 +4,8 @@ function [X, w, h] = LoadData(DirName, p)
     X = zeros(w*h,numel(pics));
     for i=1:numel(pics)
         if(d~=1)
-            cp = rgb2gray(double(GrabCenterPixels(strcat(DirName,'/',pics(i).name), p))/256);
+            cp = rgb2gray(double(GrabCenterPixels(strcat(DirName,'/',pics(i).name), ...
+            p))/256);
         else
             cp = double(GrabCenterPixels(strcat(DirName,'/',pics(i).name), p))/256;
         end
