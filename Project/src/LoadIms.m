bd64068290006f45c7f8dbedcd0993dec0f96b61
Jim Holmström <jim.holmstroem@gmail.com>
2012-05-02 12:09:04 +0200

    added a stop at LoadIms since it has hardcoded number 100

--- Project/src/LoadIms.m
+++ Project/src/LoadIms.m
@@ -1,5 +1,6 @@
 function ii_ims = LoadIms(dir_name)
     pics = dir(strcat(dir_name,'/*.bmp'));
+    no hardcoded numbers dude
     ii_ims = cell(1,100);
      for i=1:100
             [im, ii_im] = LoadIm(pics(i).name);
