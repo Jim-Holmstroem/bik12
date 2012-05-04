93fd0a9d357c192254920b33d6cdb7732d2c42b6
Jim Holmström <jim.holmstroem@gmail.com>
2012-05-04 16:47:29 +0200

    Rremoved unnecssary output

--- Project/src/MakeFeaturePic.m
+++ Project/src/MakeFeaturePic.m
@@ -13,9 +13,9 @@ function fpic = MakeFeaturePic(ftype, W, H)
             fpic(y:y+h,x:x+w) = -1;
             fpic(y:y+h,x+w:x+2*w-1) = 1;
         case 3
-            fpic=MakeFeaturePic([2,x,y,w,h],W,H)
+            fpic=MakeFeaturePic([2,x,y,w,h],W,H);
             fpic(y:y+h,x+2*w:x+3*w-1) = -1;
         case 4
-            fpic=MakeFeaturePic([2,x,y,w,h],W,H)-4*MakeFeaturePic([2,x,y+h+1,w,h],W,H)
+            fpic=MakeFeaturePic([2,x,y,w,h],W,H)-4*MakeFeaturePic([2,x,y+h+1,w,h],W,H);
     end
 end
