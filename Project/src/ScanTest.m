dd4917340338ad3f1581ea3bef87113ffb9ece4f
Jim Holmström <jim.holmstroem@gmail.com>
2012-06-09 20:39:50 +0200

    save

--- Project/src/ScanTest.m
+++ Project/src/ScanTest.m
@@ -5,11 +5,14 @@ ii_im2=[zeros(1,h+1);[zeros(w,1),cumsum(cumsum(im2,1),2)]];
 
 L=19;
 
+theta = 20;
+
 for x=1:(W-L)
     for y=1:(H-L)
         %local normalization
         mu     = VecComputeFeature(ii_im,VecBoxSum(x,y,L,L,W,H)/(L^2);
-        sigma2 = (VecComputeFeatute(ii_im2,VecBoxSum(x,y,L,L,W,H))-(L*mu)^2)/(L^2-1);
+        sigma2 = (VecComputeFeature(ii_im2,VecBoxSum(x,y,L,L,W,H))-(L*mu)^2)/(L^2-1);
+        
 
     end
 end
